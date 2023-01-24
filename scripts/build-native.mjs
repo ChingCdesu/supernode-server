eb4ccd912d39f6b97676e0d3124520a5f9136854
_ChingC <chingc.ccmail@gmail.com>
2023-01-25 00:19:24 +0800

    fix: node headers not found

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const { CMake } = CMakeJs;
+const { BuildSystem } = CMakeJs;
 
 const { NATIVE_BUILD_TYPE } = process.env;
 
@@ -14,12 +14,12 @@ const projectRoot = path.join(__dirname, '../native');
 const buildDir = path.join(projectRoot, 'build');
 const buildType = NATIVE_BUILD_TYPE || 'Release';
 
-const cmake = new CMake({
+const bs = new BuildSystem({
   directory: projectRoot,
   config: buildType,
 });
 
-cmake.compile().then(() => {
+bs.compile().then(() => {
   const targetFile = path.resolve(buildDir, buildType, 'node-supernode.node');
   const exists = fs.existsSync(targetFile);
   if (exists) {
