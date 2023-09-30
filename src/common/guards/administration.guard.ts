b7881e8f6eb47dbe644482cffd6f04cfe13acb60
_ChingC <chingc.ccmail@gmail.com>
2023-10-01 01:07:14 +0800

    fix: unable to get correct user privilege from oidc

--- src/common/guards/administration.guard.ts
+++ src/common/guards/administration.guard.ts
@@ -1,10 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 
+import { User } from '@/modules/user/entities/user.entity';
+import { useConfig } from '@/utils/config.util';
+
 @Injectable()
 export class AdministrationGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     const request: Request = context.switchToHttp().getRequest();
-    return !!request.user?.isAdmin;
+    if (!request.user) {
+      return false;
+    }
+    if (request.user instanceof User) {
+      return request.user.isAdmin;
+    } else if (request.user.sub) {
+      const config = useConfig();
+      return Array.from((request.user.groups as string[]) ?? []).includes(
+        config.oidc.adminGroup,
+      );
+    }
+    return false;
   }
 }
